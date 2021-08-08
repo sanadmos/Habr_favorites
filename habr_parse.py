57f6b6ba1b86d75900064902a7ef5cfd1357e36c
admOS <kolobokteam@yandex.ru>
2021-08-08 17:29:09 +0300

    123

--- habr_parse.py
+++ habr_parse.py
@@ -5,48 +5,64 @@ Created on Sat Jun 19 17:28:06 2021
 @author: admOS
 """
 
-import requests
-from lxml import html
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
 import openpyxl
+import datetime
 
-pages = []
-url = "https://habr.com/ru/users/admos/favorites/posts"
-page = requests.get(url).content.decode("utf-8")
-pages.append(page)
-tree = html.fromstring(page)
-pgs = len(tree.xpath('.//a/@href[class="tm-pagination__page"]'))
-for num in range(2, pgs+1):
-    url = f"https://habr.com/ru/users/admos/favorites/page{num}"
-    page = requests.get(url).content.decode("utf-8")
-    pages.append(page)
+def read_page(num):
+    url = f"https://habr.com/ru/users/admos/favorites/posts/page{num}/"
+    page = urlopen(url)
+    page_obj = BeautifulSoup(page.read())
+    return page_obj
+
+def convert_str_to_datetime(datetime_str):
+    """
+    Конвертирует строку с датой в формате 2013-09-10, 20:56 в объект datetime.
+    """
+    return datetime.datetime.strptime(datetime_str, "%Y-%m-%d, %H:%M")
+
+def convert_datetime_to_str(datetime_obj):
+    """
+    Конвертирует объект datetime в строку с датой в формате 10 September 2013.
+    """
+    return datetime.datetime.strftime(datetime_obj, "%d %B %Y")
 
 xl = openpyxl.Workbook()
 ws = xl.active
 ws.title = "habr.com"
-row = 0
-for page in pages:
-    tree = html.fromstring(page)
-    articles = tree.xpath('//article[@class="post post_preview"]')
+row = 1
+page_obj = read_page(1)
+num_pages = int(page_obj.find("div", {"class": "tm-pagination__pages"}).findAll("a", {"class": "tm-pagination__page"})[-1].get_text().strip())
+for num in range(1, num_pages+1):
+    page_obj = read_page(num)
+    articles = page_obj.findAll("article", {"class": "tm-articles-list__item"})
     for article in articles:
-        row +=1
+        date = article.find("time").attrs["title"]                             # время публикации поста
+        date = convert_datetime_to_str(convert_str_to_datetime(date))
+        title_obj = article.find("h2", {"class": "tm-article-snippet__title tm-article-snippet__title_h2"})
+        title = title_obj.find("span").get_text().strip()                      # название поста
+        link = "https://habr.com" + title_obj.find("a").attrs["href"]          # ссылка на пост
+        hubs_obj = article.find("div", {"class": "tm-article-snippet__hubs"})
+        labels_obj = article.find("div", {"class": "tm-article-snippet__labels"})
         try:
-            date = article.xpath('.//span[@class="post__time"]')[0].text # время публикации поста
-            title = article.xpath('.//h2')[0][0].text # название поста
-            link = article.xpath('.//a/@href')[1] # ссылка на пост
-            hubs = article.xpath('.//li[@class="inline-list__item inline-list__item_hub"]') # категория поста
-            hubs = [hub[0].text for hub in hubs if "Блог компании" not in hub[0].text]
-            types = article.xpath('.//span[@class="post__type-label"]') # тип поста
-            types = [taip.text for taip in types]
-            # if ("IT-эмиграция" in hubs) or ("Карьера в IT-индустрии" in hubs) or ("Образование за рубежом" in hubs):
-            ws.cell(row=row, column=1).style = "Hyperlink"
-            ws.cell(row=row, column=1).value = title
-            ws.cell(row=row, column=1).hyperlink = link
-            ws.cell(row=row, column=2, value=", ".join(hubs))
-            ws.cell(row=row, column=3, value=", ".join(types))
-            ws.cell(row=row, column=4, value=date)
-        except IndexError:
-            continue
+            hubs = [hub.get_text().strip().replace("*", "") for hub in hubs_obj if "Блог компании" not in hub.get_text()]
+        except AttributeError:
+            hubs = []
+        try:
+            labels = [label.get_text().strip() for label in labels_obj]
+        except AttributeError:
+            labels = []
+        ws.cell(row=row, column=1).style = "Hyperlink"
+        ws.cell(row=row, column=1).value = title
+        ws.cell(row=row, column=1).hyperlink = link
+        ws.cell(row=row, column=2, value=", ".join(hubs))
+        ws.cell(row=row, column=3, value=", ".join(labels))
+        ws.cell(row=row, column=4, value=date)
+        row +=1
 ws.column_dimensions["A"].width = 130
-ws.column_dimensions["B"].width = 30
+ws.column_dimensions["B"].width = 112
+ws.column_dimensions["C"].width = 32
+ws.column_dimensions["D"].width = 20
 xl.save("habr.com.xlsx")
 xl.close()
