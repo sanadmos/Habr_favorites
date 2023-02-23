ed53c064278511d46ffca0124275f1bf1ffa3d75
admOS <kolobokteam@yandex.ru>
2023-02-23 10:45:53 +0300

    update of code

--- habr_parse.py
+++ habr_parse.py
@@ -10,11 +10,11 @@ from bs4 import BeautifulSoup
 import openpyxl
 import datetime
 
-def read_page(num):
-    url = f"https://habr.com/ru/users/admos/favorites/posts/page{num}/"
+def read_page(login, number):
+    url = f"https://habr.com/ru/users/{login}/favorites/posts/page{number}/"
     page = urlopen(url)
-    page_obj = BeautifulSoup(page.read())
-    return page_obj
+    page_object = BeautifulSoup(page.read())
+    return page_object
 
 def convert_str_to_datetime(datetime_str):
     """
@@ -28,23 +28,31 @@ def convert_datetime_to_str(datetime_obj):
     """
     return datetime.datetime.strftime(datetime_obj, "%d %B %Y")
 
+username = input("Введите имя пользователя на habr.com\n")
 xl = openpyxl.Workbook()
 ws = xl.active
 ws.title = "habr.com"
 row = 1
-page_obj = read_page(1)
+page_obj = read_page(username, 1)
 num_pages = int(page_obj.find("div", {"class": "tm-pagination__pages"}).findAll("a", {"class": "tm-pagination__page"})[-1].get_text().strip())
 for num in range(1, num_pages+1):
-    page_obj = read_page(num)
+    page_obj = read_page(username, num)
     articles = page_obj.findAll("article", {"class": "tm-articles-list__item"})
     for article in articles:
         date = article.find("time").attrs["title"]                             # время публикации поста
         date = convert_datetime_to_str(convert_str_to_datetime(date))
         title_obj = article.find("h2", {"class": "tm-article-snippet__title tm-article-snippet__title_h2"})
-        title = title_obj.find("span").get_text().strip()                      # название поста
-        link = "https://habr.com" + title_obj.find("a").attrs["href"]          # ссылка на пост
-        hubs_obj = article.find("div", {"class": "tm-article-snippet__hubs"})
-        labels_obj = article.find("div", {"class": "tm-article-snippet__labels"})
+        if title_obj:
+            title = title_obj.find("span").get_text().strip()  # название поста
+            link = "https://habr.com" + title_obj.find("a").attrs["href"]  # ссылка на пост
+            hubs_obj = article.find("div", {"class": "tm-article-snippet__hubs"})
+            labels_obj = article.find("div", {"class": "tm-article-snippet__labels"})
+        else:
+            title_obj = article.find("h2", {"class": "tm-megapost-snippet__title"})
+            title = title_obj.get_text().strip()  # название поста
+            link = "https://habr.com" + article.find("a", {"class": "tm-megapost-snippet__link tm-megapost-snippet__card"}).attrs["href"]  # ссылка на пост
+            hubs_obj = article.find("ul", {"class": "tm-megapost-snippet__hubs"})
+            labels_obj = article.find("div", {"class": "tm-megapost-snippet__labels"})
         try:
             hubs = [hub.get_text().strip().replace("*", "") for hub in hubs_obj if "Блог компании" not in hub.get_text()]
         except AttributeError:
